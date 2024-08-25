d276f23dce2252b5e88711c4b7039d56678012e9
rajanbajaj <rajanbajajkota@gmail.com>
2024-08-25 13:50:48 +0530

    Add Candlestick patterns result on dashboard UI

--- dashboard/static/js/all.js
+++ dashboard/static/js/all.js
@@ -37,7 +37,114 @@ function loadOILosers() {
         console.log("ERROR IN OI LOSERS.");
     }
 }
+
+function loadBullishEngullfing() {
+    try {
+        fetch(`${API_BASE_URL}/bullish-engullfing`).then((response) => {
+            response.json().then((value) => {
+                const list = createList(value.data);
+                document.getElementById('bullish-engullfing').innerHTML = '';
+                document.getElementById('bullish-engullfing').appendChild(list);
+            })
+        });
+    } catch {
+        console.log("ERROR IN BULLISH ENGULLFING");
+    }
+}
+
+function loadDoji() {
+    try {
+        fetch(`${API_BASE_URL}/doji`).then((response) => {
+            response.json().then((value) => {
+                const list = createList(value.data);
+                document.getElementById('doji').innerHTML = '';
+                document.getElementById('doji').appendChild(list);
+            })
+        });
+    } catch {
+        console.log("ERROR IN DOJI.");
+    }
+}
+
+function loadFallingWedge() {
+    try {
+        fetch(`${API_BASE_URL}/falling-wedge`).then((response) => {
+            response.json().then((value) => {
+                const list = createList(value.data);
+                document.getElementById('falling-wedge').innerHTML = '';
+                document.getElementById('falling-wedge').appendChild(list);
+            })
+        });
+    } catch {
+        console.log("ERROR IN FALLING WEDGE.");
+    }
+}
+
+function loadRisingWedge() {
+    try {
+        fetch(`${API_BASE_URL}/rising-wedge`).then((response) => {
+            response.json().then((value) => {
+                const list = createList(value.data);
+                document.getElementById('rising-wedge').innerHTML = '';
+                document.getElementById('rising-wedge').appendChild(list);
+            })
+        });
+    } catch {
+        console.log("ERROR IN RISING WEDGE.");
+    }
+}
+function loadHammer() {
+    try {
+        fetch(`${API_BASE_URL}/hammer`).then((response) => {
+            response.json().then((value) => {
+                const list = createList(value.data);
+                document.getElementById('hammer').innerHTML = '';
+                document.getElementById('hammer').appendChild(list);
+            })
+        });
+    } catch {
+        console.log("ERROR IN HAMMER.");
+    }
+}
+
+function loadNear52WeekHigh() {
+    try {
+        fetch(`${API_BASE_URL}/near-52week-high`).then((response) => {
+            response.json().then((value) => {
+                const list = createList(value.data);
+                document.getElementById('near-52week-high').innerHTML = '';
+                document.getElementById('near-52week-high').appendChild(list);
+            })
+        });
+    } catch {
+        console.log("ERROR IN NER 52 WEEKS HIGH.");
+    }
+}
+
+function loadNear52WeekLow() {
+    try {
+        fetch(`${API_BASE_URL}/near-52week-low`).then((response) => {
+            response.json().then((value) => {
+                const list = createList(value.data);
+                document.getElementById('near-52week-low').innerHTML = '';
+                document.getElementById('near-52week-low').appendChild(list);
+            })
+        });
+    } catch {
+        console.log("ERROR IN NEAR 52 WEEKS LOW.");
+    }
+}
+
+
+
 loadOIGainers()
 loadOILosers()
+loadBullishEngullfing()
+loadDoji()
+loadFallingWedge()
+loadRisingWedge()
+loadHammer()
+loadNear52WeekHigh()
+loadNear52WeekLow()
 setInterval(loadOIGainers, 60000);
 setInterval(loadOILosers, 60000);
